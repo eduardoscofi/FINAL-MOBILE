8f5526fe36954a08ac5f67b5a8b93effb931c130
Eduardo Scofi <144451305+eduardoscofi@users.noreply.github.com>
2024-07-01 19:42:37 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// Importa o componente que define a navegação da aplicação
+import AppNavigator from './AppNavigator'; 
+
+// Componente principal que inicializa a navegação do aplicativo
+const App = () => {
+  return <AppNavigator />; // Retorna o componente AppNavigator que contém a estrutura de navegação da aplicação
+};
+
+export default App;
